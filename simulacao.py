8ae84682b9c1622f2686491ddc7d8fcb2f8473f1
Átila Prudente <150717036+Chossei@users.noreply.github.com>
2025-04-24 23:37:18 -0300

    Update simulacao.py

--- simulacao.py
+++ simulacao.py
@@ -5,11 +5,11 @@ import streamlit as st
 import random
 
 
-def simulacao():
+def simulacao(amostra, populacao):
       # Com reposição
       medias_cr = []
-      tiras = [1, 2, 3, 4]
-      for i in range(1, 5):
+      tiras = range(1, populacao+1)
+      for i in range(1, amostra+1):
         for j in range(1, 2501):
           lista_cr = random.choices(population = tiras, weights = None,
                             cum_weights = None, k = i)
@@ -17,9 +17,9 @@ def simulacao():
 
       # Sem reposição
       medias_sr = []
-      tiras = [1,2,3,4]
+      tiras = range(1, populacao+1)
 
-      for i in range(1, 5):
+      for i in range(1, amostra+1):
         for j in range(1, 2501):
           lista_sr = random.sample(population = tiras, k = i)
           medias_sr.append(np.mean(lista_sr))
@@ -34,35 +34,6 @@ def histograma(data):
   plt.close(fig)
   return fig
 
-# definindo as variáveis e as figuras
-reposicao, sem_reposicao = simulacao()
-fig_rep = histograma(reposicao)
-fig_srep = histograma(sem_reposicao)
-
-# Calculando as métricas ----------------------
-# Com reposição
-minimo_cr = float(np.min(reposicao))
-esperado_cr = float(np.mean(reposicao))
-mediana_cr = float(np.median(reposicao))
-dp_cr = float(np.std(reposicao))
-max_cr = float(np.max(reposicao))
-
-# Sem reposição
-
-minimo_sr = float(np.min(sem_reposicao))
-esperado_sr = float(np.mean(sem_reposicao))
-mediana_sr = float(np.median(sem_reposicao))
-dp_sr = float(np.std(sem_reposicao))
-max_sr = float(np.max(sem_reposicao))
-
-# Cálculo da diferença para uso no st.metric
-
-metric_min = round(minimo_sr - minimo_cr, ndigits=2)
-metric_mean = round(esperado_sr - esperado_cr, ndigits=2)
-metric_median = round(mediana_sr - mediana_cr, ndigits=2)
-metric_dp = round(dp_sr - dp_cr, ndigits=2)
-metric_max = round(max_sr - max_cr, ndigits=2)
-
 # ---------------------------------------------
 # Criando a estrutura do programa
 
@@ -76,83 +47,128 @@ st.set_page_config(
 st.title('🎲 Simulação - Amostragem A - 2025.1')
 st.markdown("""
 <div style='text-align: justify'>
-<p>Neste experimento, realizamos amostragens com e sem reposição a partir de uma caixa contendo quatro números: {1, 2, 3, 4}. 
-Foram selecionadas amostras de tamanhos variando entre 1 e 4, e para cada tamanho, foram feitas 250 repetições, totalizando 1000 amostras em cada tipo de amostragem.
+<p>Neste experimento, realizamos amostragens com e sem reposição a partir de uma caixa contendo <b>N</b> números diferentes, representando a população. Por exemplo, se N = 4, a população será {1, 2, 3, 4}. 
 
-<p>Para cada amostra, calculou-se a média dos valores selecionados. Em seguida, as distribuições dessas médias foram analisadas por meio de métricas descritivas e histogramas comparativos.
+<p> Para cada tamanho de amostra de 1 até <b>n</b>, foram realizadas 2500 repetições, totalizando <b>n × 2500</b> amostras para cada tipo de amostragem.
+            
+<p>Em cada repetição, calcula-se a média dos valores selecionados. As distribuições dessas médias são então analisadas por meio de métricas descritivas e histogramas comparativos.
 </div>
 """, unsafe_allow_html=True)
 
 st.divider()
 
-st.subheader('🔁 Com reposição')
+amos, popu = st.columns(2, gap = 'large')
 
-c1, c2, c3, c4, c5 = st.columns(5)
+with amos:
+      amostra = st.slider(label = 'Selecione o tamanho (acumulativo) da amostra:', min_value = 1,
+                    max_value = 10, value = 3, step = 1)
+with popu:
+   populacao = st.slider(label = 'Selecione o tamanho da população:', min_value = 1,
+                    max_value = 10, value = 5, step = 1)
 
-with c1:
-   st.metric(label = 'Mínimo',
-      value = round(minimo_cr, ndigits = 4))
+# definindo as variáveis e as figuras
+try: 
+   reposicao, sem_reposicao = simulacao(amostra, populacao)
+   fig_rep = histograma(reposicao)
+   fig_srep = histograma(sem_reposicao)
 
-with c2:
-   st.metric(label = 'Valor esperado',
-      value = round(esperado_cr, ndigits = 4))
+   # Calculando as métricas ----------------------
+   # Com reposição
+   minimo_cr = float(np.min(reposicao))
+   esperado_cr = float(np.mean(reposicao))
+   mediana_cr = float(np.median(reposicao))
+   dp_cr = float(np.std(reposicao))
+   max_cr = float(np.max(reposicao))
 
-with c3:
-   st.metric(label = 'Mediana',
-      value = round(mediana_cr, ndigits = 4))
+   # Sem reposição
 
-with c4:
-   st.metric(label = 'Desvio padrão',
-      value = round(dp_cr, ndigits = 4))
+   minimo_sr = float(np.min(sem_reposicao))
+   esperado_sr = float(np.mean(sem_reposicao))
+   mediana_sr = float(np.median(sem_reposicao))
+   dp_sr = float(np.std(sem_reposicao))
+   max_sr = float(np.max(sem_reposicao))
 
-with c5:
-   st.metric(label = 'Máximo',
-      value = round(max_cr, ndigits = 4))
-   
-st.divider()
+   # Cálculo da diferença para uso no st.metric
 
-st.subheader('🚫 Sem reposição')
+   metric_min = round(minimo_sr - minimo_cr, ndigits=2)
+   metric_mean = round(esperado_sr - esperado_cr, ndigits=2)
+   metric_median = round(mediana_sr - mediana_cr, ndigits=2)
+   metric_dp = round(dp_sr - dp_cr, ndigits=2)
+   metric_max = round(max_sr - max_cr, ndigits=2)
 
-s1, s2, s3, s4, s5 = st.columns(5)
+   # Reproduzindo os resultados ---------------
 
-with s1:
-   st.metric(label = 'Mínimo',
-      value = round(minimo_sr, ndigits = 4),
-      delta=metric_min)
+   st.subheader('🔁 Com reposição')
 
-with s2:
-   st.metric(label = 'Valor esperado',
-      value = round(esperado_sr, ndigits = 4),
-      delta=metric_mean)
+   c1, c2, c3, c4, c5 = st.columns(5)
 
-with s3:
-   st.metric(label = 'Mediana',
-      value = round(mediana_sr, ndigits = 4),
-      delta=metric_median)
+   with c1:
+      st.metric(label = 'Mínimo',
+         value = round(minimo_cr, ndigits = 4))
 
-with s4:
-   st.metric(label = 'Desvio padrão',
-      value = round(dp_sr, ndigits = 4),
-      delta=metric_dp)
+   with c2:
+      st.metric(label = 'Valor esperado',
+         value = round(esperado_cr, ndigits = 4))
 
-with s5:
-   st.metric(label = 'Máximo',
-      value = round(max_sr, ndigits = 4),
-      delta=metric_max)
+   with c3:
+      st.metric(label = 'Mediana',
+         value = round(mediana_cr, ndigits = 4))
+
+   with c4:
+      st.metric(label = 'Desvio padrão',
+         value = round(dp_cr, ndigits = 4))
+
+   with c5:
+      st.metric(label = 'Máximo',
+         value = round(max_cr, ndigits = 4))
+      
+   st.divider()
+
+   st.subheader('🚫 Sem reposição')
+
+   s1, s2, s3, s4, s5 = st.columns(5)
+
+   with s1:
+      st.metric(label = 'Mínimo',
+         value = round(minimo_sr, ndigits = 4),
+         delta=metric_min)
+
+   with s2:
+      st.metric(label = 'Valor esperado',
+         value = round(esperado_sr, ndigits = 4),
+         delta=metric_mean)
+
+   with s3:
+      st.metric(label = 'Mediana',
+         value = round(mediana_sr, ndigits = 4),
+         delta=metric_median)
+
+   with s4:
+      st.metric(label = 'Desvio padrão',
+         value = round(dp_sr, ndigits = 4),
+         delta=metric_dp)
+
+   with s5:
+      st.metric(label = 'Máximo',
+         value = round(max_sr, ndigits = 4),
+         delta=metric_max)
 
 
-# Exibição dos gráficos
+   # Exibição dos gráficos
 
-st.divider()
+   st.divider()
 
-hist1, hist2 = st.columns(2, gap = 'medium',
-                          vertical_alignment='center',
-                          border=False)
+   hist1, hist2 = st.columns(2, gap = 'medium',
+                           vertical_alignment='center',
+                           border=False)
 
-with hist1:
-   st.subheader('Distribuição das médias com reposição')
-   st.pyplot(fig_rep)
+   with hist1:
+      st.markdown("<h3 style='text-align: center;'>Distribuição das médias com reposição</h3>", unsafe_allow_html=True)
+      st.pyplot(fig_rep)
 
-with hist2:
-   st.subheader('Distribuição das médias sem reposição')
-   st.pyplot(fig_srep)
+   with hist2:
+      st.markdown("<h3 style='text-align: center;'>Distribuição das médias sem reposição</h3>", unsafe_allow_html=True)
+      st.pyplot(fig_srep)
+
+except ValueError:
+      st.error('🚨 *ATENÇÃO*: o tamanho da amostra **não** pode ser maior que o da população!')
